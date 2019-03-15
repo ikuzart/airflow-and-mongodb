0e2002c5ea1a32c5b2e7758f0437861d0984a986
Arthur Kuziakhmetov <ikuzart@gmail.com>
2019-03-15 18:13:35 +0300

    Add model builder

--- dags/dag.py
+++ dags/dag.py
@@ -1,14 +1,14 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from airflow.operators.dummy_operator import DummyOperator
 
 from data import DataContainer
 from data_preprocessing import BreastCancerData, BreastCancerDataLoader, BreastCancerDataPreprocessor
+from model_building import ModelBuilder
 from mongo_wrapper import MongoClientWrapper
 from settings import TEST_SIZE, RANDOM_STATE, MONGO_DB, MONGO_COLLECTION, default_args
 
 
-def data_preprocessing():
+def data_preprocessing(**kwargs):
     data_container = DataContainer()
     mongo_client_wrapper = MongoClientWrapper()
 
@@ -17,20 +17,39 @@ def data_preprocessing():
     preproc = BreastCancerDataPreprocessor(data_to_preprocess, data_container)
     preprocessed_data = preproc.preprocess(TEST_SIZE, RANDOM_STATE)
     document_to_save = mongo_client_wrapper.make_document_from_data_container(preprocessed_data)
-    mongo_client_wrapper.insert_one(document_to_save, MONGO_DB, MONGO_COLLECTION)
+    id_ = mongo_client_wrapper.insert_one(document_to_save, MONGO_DB, MONGO_COLLECTION)
+    return id_.inserted_id
 
-    return
 
+def model_training(**kwargs):
+    ti = kwargs['task_instance']
+    id_ = ti.xcom_pull(task_ids='data_preprocessing')
+    print(id_)
+    mongo_client_wrapper = MongoClientWrapper()
+    data_container = DataContainer()
 
-def model_training():
-    return 'model training'
+    document = mongo_client_wrapper.find_by_id(id_,  MONGO_DB, MONGO_COLLECTION)
+    print(document)
+    model_builder = ModelBuilder(*mongo_client_wrapper.get_train_test_data_from_document(document))
+    data_container.classifier = model_builder.build_classifier()
+    data_container.prediction = model_builder.prediction(data_container.classifier)
+    data_container.metrics = model_builder.get_metrics(data_container.prediction)
+    document_to_save = mongo_client_wrapper.make_document_from_data_container(data_container)
+
+    mongo_client_wrapper.update_one(id_, MONGO_DB, MONGO_COLLECTION, document_to_save)
 
 
 dag = DAG('test_project', default_args=default_args, schedule_interval=None)
 
 
-preprocessing_operator = PythonOperator(task_id='data_preprocessing', python_callable=data_preprocessing, dag=dag)
+preprocessing_operator = PythonOperator(task_id='data_preprocessing',
+                                        python_callable=data_preprocessing,
+                                        provide_context=True,
+                                        dag=dag)
 
-model_training_operator = DummyOperator(task_id='model_training',  dag=dag)
+model_training_operator = PythonOperator(task_id='model_training',
+                                         provide_context=True,
+                                         python_callable=model_training,
+                                         dag=dag)
 
 preprocessing_operator >> model_training_operator
